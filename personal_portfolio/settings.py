8a26031bb44fb37f1cf4b7a7977db84f847cbe37
c4nadian <to0oni456@gmail.com>
2019-07-17 14:40:32 +0200

    ,

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -129,12 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-#STATIC_URL = '/static/'
-
-#STATICFILES_DIRS = [
-#   os.path.join(BASE_DIR, "static"),
-#    '/static/',
-#]
+STATIC_URL = '/static/'
 
 AUTHENTICATION_BACKENDS = (
     'social_core.backends.spotify.SpotifyOAuth2',
